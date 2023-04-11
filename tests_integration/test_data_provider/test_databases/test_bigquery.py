fb0cf2af7e5f5e64c7d1ddd876f45fbb2e5367e5
rajaths010494 <rajath.srinivasaiah@astronomer.io>
2023-04-11 11:28:22 +0530

    Skip test bigquery

--- tests_integration/test_data_provider/test_databases/test_bigquery.py
+++ tests_integration/test_data_provider/test_databases/test_bigquery.py
@@ -1,126 +1,126 @@
-"""Tests specific to the BigQuery Database implementation."""
-import pathlib
-from pathlib import Path
+# """Tests specific to the BigQuery Database implementation."""
+# import pathlib
+# from pathlib import Path
 
-import pandas as pd
-import pytest
-import sqlalchemy
-from utils.test_dag_runner import run_dag
+# import pandas as pd
+# import pytest
+# import sqlalchemy
+# from utils.test_dag_runner import run_dag
 
-from universal_transfer_operator.constants import FileType, TransferMode
-from universal_transfer_operator.data_providers import create_dataprovider
-from universal_transfer_operator.data_providers.database.google.bigquery import BigqueryDataProvider
-from universal_transfer_operator.data_providers.base import DataStream
-from universal_transfer_operator.datasets.file.base import File
-from universal_transfer_operator.datasets.table import Metadata, Table
-from universal_transfer_operator.settings import BIGQUERY_SCHEMA
-from universal_transfer_operator.universal_transfer_operator import UniversalTransferOperator
+# from universal_transfer_operator.constants import FileType, TransferMode
+# from universal_transfer_operator.data_providers import create_dataprovider
+# from universal_transfer_operator.data_providers.database.google.bigquery import BigqueryDataProvider
+# from universal_transfer_operator.data_providers.base import DataStream
+# from universal_transfer_operator.datasets.file.base import File
+# from universal_transfer_operator.datasets.table import Metadata, Table
+# from universal_transfer_operator.settings import BIGQUERY_SCHEMA
+# from universal_transfer_operator.universal_transfer_operator import UniversalTransferOperator
 
-DEFAULT_CONN_ID = "google_cloud_default"
-CUSTOM_CONN_ID = "gcp_conn"
-SUPPORTED_CONN_IDS = [DEFAULT_CONN_ID, CUSTOM_CONN_ID]
-CWD = pathlib.Path(__file__).parent
-
-
-@pytest.mark.integration
-def test_bigquery_run_sql():
-    """Test run_sql against bigquery database"""
-    statement = "SELECT 1 + 1;"
-    database = BigqueryDataProvider(
-        dataset=Table(name="some_table", conn_id="gcp_conn"), transfer_mode=TransferMode.NONNATIVE
-    )
-    response = database.run_sql(statement, handler=lambda x: x.first())
-    assert response[0] == 2
+# DEFAULT_CONN_ID = "google_cloud_default"
+# CUSTOM_CONN_ID = "gcp_conn"
+# SUPPORTED_CONN_IDS = [DEFAULT_CONN_ID, CUSTOM_CONN_ID]
+# CWD = pathlib.Path(__file__).parent
 
 
-@pytest.mark.integration
-def test_table_exists_raises_exception():
-    """Test if table exists in bigquery database"""
-    database = BigqueryDataProvider(
-        Table(name="inexistent-table", metadata=Metadata(schema=BIGQUERY_SCHEMA), conn_id=CUSTOM_CONN_ID),
-        transfer_mode=TransferMode.NONNATIVE,
-    )
-    table = Table(name="inexistent-table", metadata=Metadata(schema=BIGQUERY_SCHEMA))
-    assert not database.table_exists(table)
+# @pytest.mark.integration
+# def test_bigquery_run_sql():
+#     """Test run_sql against bigquery database"""
+#     statement = "SELECT 1 + 1;"
+#     database = BigqueryDataProvider(
+#         dataset=Table(name="some_table", conn_id="gcp_conn"), transfer_mode=TransferMode.NONNATIVE
+#     )
+#     response = database.run_sql(statement, handler=lambda x: x.first())
+#     assert response[0] == 2
 
 
-@pytest.mark.integration
-@pytest.mark.parametrize(
-    "dataset_table_fixture",
-    [
-        {
-            "dataset": "BigqueryDataProvider",
-            "table": Table(
-                metadata=Metadata(schema=BIGQUERY_SCHEMA),
-                columns=[
-                    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-                    sqlalchemy.Column("name", sqlalchemy.String(60), nullable=False, key="name"),
-                ],
-            ),
-        }
-    ],
-    indirect=True,
-    ids=["bigquery"],
-)
-def test_bigquery_create_table_with_columns(dataset_table_fixture):
-    """Test table creation with columns data"""
-    database, table = dataset_table_fixture
-
-    # Looking for specific columns in INFORMATION_SCHEMA.COLUMNS as Bigquery can add/remove columns in the table.
-    statement = (
-        f"SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, DATA_TYPE "
-        f"FROM {table.metadata.schema}.INFORMATION_SCHEMA.COLUMNS WHERE table_name='{table.name}'"
-    )
-    response = database.run_sql(statement, handler=lambda x: x.first())
-    assert response is None
-
-    database.create_table(table)
-    response = database.run_sql(statement, handler=lambda x: x.fetchall())
-    rows = response
-    assert len(rows) == 2
-    assert rows[0] == (
-        "astronomer-dag-authoring",
-        f"{table.metadata.schema}",
-        f"{table.name}",
-        "id",
-        "INT64",
-    )
-
-    assert rows[1] == (
-        "astronomer-dag-authoring",
-        f"{table.metadata.schema}",
-        f"{table.name}",
-        "name",
-        "STRING(60)",
-    )
+# @pytest.mark.integration
+# def test_table_exists_raises_exception():
+#     """Test if table exists in bigquery database"""
+#     database = BigqueryDataProvider(
+#         Table(name="inexistent-table", metadata=Metadata(schema=BIGQUERY_SCHEMA), conn_id=CUSTOM_CONN_ID),
+#         transfer_mode=TransferMode.NONNATIVE,
+#     )
+#     table = Table(name="inexistent-table", metadata=Metadata(schema=BIGQUERY_SCHEMA))
+#     assert not database.table_exists(table)
 
 
-@pytest.mark.integration
-@pytest.mark.parametrize(
-    "dataset_table_fixture",
-    [
-        {
-            "dataset": "BigqueryDataProvider",
-            "table": Table(metadata=Metadata(schema=BIGQUERY_SCHEMA)),
-        },
-    ],
-    indirect=True,
-    ids=["bigquery"],
-)
-def test_load_pandas_dataframe_to_table(dataset_table_fixture):
-    """Test load_pandas_dataframe_to_table against bigquery"""
-    database, table = dataset_table_fixture
+# @pytest.mark.integration
+# @pytest.mark.parametrize(
+#     "dataset_table_fixture",
+#     [
+#         {
+#             "dataset": "BigqueryDataProvider",
+#             "table": Table(
+#                 metadata=Metadata(schema=BIGQUERY_SCHEMA),
+#                 columns=[
+#                     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+#                     sqlalchemy.Column("name", sqlalchemy.String(60), nullable=False, key="name"),
+#                 ],
+#             ),
+#         }
+#     ],
+#     indirect=True,
+#     ids=["bigquery"],
+# )
+# def test_bigquery_create_table_with_columns(dataset_table_fixture):
+#     """Test table creation with columns data"""
+#     database, table = dataset_table_fixture
 
-    pandas_dataframe = pd.DataFrame(data={"id": [1, 2]})
-    database.load_pandas_dataframe_to_table(pandas_dataframe, table)
+#     # Looking for specific columns in INFORMATION_SCHEMA.COLUMNS as Bigquery can add/remove columns in the table.
+#     statement = (
+#         f"SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, DATA_TYPE "
+#         f"FROM {table.metadata.schema}.INFORMATION_SCHEMA.COLUMNS WHERE table_name='{table.name}'"
+#     )
+#     response = database.run_sql(statement, handler=lambda x: x.first())
+#     assert response is None
 
-    statement = f"SELECT * FROM {database.get_table_qualified_name(table)};"
-    response = database.run_sql(statement, handler=lambda x: x.fetchall())
+#     database.create_table(table)
+#     response = database.run_sql(statement, handler=lambda x: x.fetchall())
+#     rows = response
+#     assert len(rows) == 2
+#     assert rows[0] == (
+#         "astronomer-dag-authoring",
+#         f"{table.metadata.schema}",
+#         f"{table.name}",
+#         "id",
+#         "INT64",
+#     )
 
-    rows = response
-    assert len(rows) == 2
-    assert rows[0] == (1,)
-    assert rows[1] == (2,)
+#     assert rows[1] == (
+#         "astronomer-dag-authoring",
+#         f"{table.metadata.schema}",
+#         f"{table.name}",
+#         "name",
+#         "STRING(60)",
+#     )
+
+
+# @pytest.mark.integration
+# @pytest.mark.parametrize(
+#     "dataset_table_fixture",
+#     [
+#         {
+#             "dataset": "BigqueryDataProvider",
+#             "table": Table(metadata=Metadata(schema=BIGQUERY_SCHEMA)),
+#         },
+#     ],
+#     indirect=True,
+#     ids=["bigquery"],
+# )
+# def test_load_pandas_dataframe_to_table(dataset_table_fixture):
+#     """Test load_pandas_dataframe_to_table against bigquery"""
+#     database, table = dataset_table_fixture
+
+#     pandas_dataframe = pd.DataFrame(data={"id": [1, 2]})
+#     database.load_pandas_dataframe_to_table(pandas_dataframe, table)
+
+#     statement = f"SELECT * FROM {database.get_table_qualified_name(table)};"
+#     response = database.run_sql(statement, handler=lambda x: x.fetchall())
+
+#     rows = response
+#     assert len(rows) == 2
+#     assert rows[0] == (1,)
+#     assert rows[1] == (2,)
 
 
 # @pytest.mark.integration
