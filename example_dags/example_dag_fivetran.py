74ea9bcd06201d958eed909de916b7d1b71de953
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2023-04-18 12:49:17 +0545

    Change the path to classes

--- example_dags/example_dag_fivetran.py
+++ example_dags/example_dag_fivetran.py
@@ -6,44 +6,18 @@ from airflow import DAG
 from universal_transfer_operator.constants import TransferMode
 from universal_transfer_operator.datasets.file.base import File
 from universal_transfer_operator.datasets.table import Metadata, Table
-from universal_transfer_operator.integrations.fivetran import Connector, Destination, FiveTranOptions, Group
+from universal_transfer_operator.integrations.fivetran.connector import FivetranConnector
+from universal_transfer_operator.integrations.fivetran.destination import FivetranDestination
+from universal_transfer_operator.integrations.fivetran.fivetran import FiveTranOptions, Group
 from universal_transfer_operator.universal_transfer_operator import UniversalTransferOperator
 
 s3_bucket = os.getenv("S3_BUCKET", "s3://astro-sdk-test")
 gcs_bucket = os.getenv("GCS_BUCKET", "gs://uto-test")
 snowflake_database = os.getenv("SNOWFLAKE_DATABASE", "dummy-database")
-snowflake_schema = os.getenv("SNOWFLAKE_SCHEMA", "dummy-schema")
+snowflake_schema = os.getenv("SNOWFLAKE_SCHEMA", "s3_test")
 
 
-connector_config = {
-    "schema": "s3",
-    "append_file_option": "upsert_file",
-    "prefix": "folder_path",
-    "pattern": "file_pattern",
-    "escape_char": "",
-    "skip_after": 0,
-    "list_strategy": "complete_listing",
-    "bucket": "astro-sdk-test",
-    "empty_header": True,
-    "skip_before": 0,
-    "role_arn": "arn::your_role_arn",
-    "file_type": "csv",
-    "delimiter": "",
-    "is_public": False,
-    "on_error": "fail",
-    "compression": "bz2",
-    "table": "fivetran_test",
-    "archive_pattern": "regex_pattern",
-}
-
-destination_config = {
-    "host": "your-account.snowflakecomputing.com",
-    "port": 443,
-    "database": "fivetran",
-    "auth": "PASSWORD",
-    "user": "fivetran_user",
-    "password": "123456",
-}
+connector_config = {"is_public": "false", "file_type": "infer", "compression": "infer", "on_error": "fail"}
 
 with DAG(
     "example_universal_transfer_operator_fivetran",
@@ -64,28 +38,22 @@ with DAG(
     # [START fivetran_transfer_without_setup]
     transfer_fivetran_without_connector_id = UniversalTransferOperator(
         task_id="transfer_fivetran_without_connector_id",
-        source_dataset=File(path=f"{s3_bucket}/uto/", conn_id="aws_default"),
+        source_dataset=File(path=f"{s3_bucket}/", conn_id="aws_default", extra={"prefix": "fivetran_test"}),
         destination_dataset=Table(
-            name="fivetran_test",
-            conn_id="snowflake_conn",
+            name="fivetran_ankit_test",
+            conn_id="snowflake_fivetran_conn",
             metadata=Metadata(database=snowflake_database, schema=snowflake_schema),
         ),
         transfer_mode=TransferMode.THIRDPARTY,
         transfer_params=FiveTranOptions(
             conn_id="fivetran_default",
-            connector_id="filing_muppet",
-            group=Group(name="test_group"),
-            connector=Connector(
-                service="s3",
-                config=connector_config,
-                connector_id=None,
-                connect_card_config={"connector_val": "test_connector"},
-            ),
-            destination=Destination(
+            group=Group(name="test_group_fivetran_snowflake"),
+            connector=FivetranConnector(service="s3", config=connector_config, connector_id=None),
+            destination=FivetranDestination(
                 service="snowflake",
                 time_zone_offset="-5",
-                region="GCP_US_EAST4",
-                config=destination_config,
+                region="US",
+                config={},
             ),
         ),
     )
