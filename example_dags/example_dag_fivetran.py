bea634a138d0250c96f9276cec43cbe630c386ad
utkarsh sharma <utkarsharma2@gmail.com>
2023-04-21 14:18:31 +0530

    Fix example testcase

--- example_dags/example_dag_fivetran.py
+++ example_dags/example_dag_fivetran.py
@@ -57,7 +57,7 @@ with DAG(
         source_dataset=File(path=f"{s3_bucket}/uto/", conn_id="aws_default"),
         destination_dataset=Table(name="fivetran_test", conn_id="snowflake_default"),
         transfer_mode=TransferMode.THIRDPARTY,
-        transfer_params=FiveTranOptions(conn_id="fivetran_default", connector_id="filing_muppet"),
+        transfer_params=FiveTranOptions(conn_id="fivetran_default", connector_id="wrap_warehouse"),
     )
     # [END fivetran_transfer_with_setup]
 
